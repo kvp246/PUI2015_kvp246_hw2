00461ba0400ef7cdcf10fdee01ab4a4eb2699fac
kvp246 <kvp246@nyu.edu>
2015-09-27 00:35:29 -0400

    updating code

--- show_bus_locations.py
+++ show_bus_locations.py
@@ -1,8 +1,11 @@
+# Importing python libraries
 import sys
 import json
 import urllib2
 
 if __name__ == '__main__':
+
+    # Calling BUS MTA API using Key and Bus Number Inputs
     url = 'http://api.prod.obanyc.com/api/siri/\
 vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&\
 LineRef=%s' % (sys.argv[1], sys.argv[2])
@@ -10,13 +13,20 @@ LineRef=%s' % (sys.argv[1], sys.argv[2])
     request = urllib2.urlopen(url)
     busData = json.load(request)
 
-    vehicleMonitoring = (
-        busData['Siri']['ServiceDelivery']
-        ['VehicleMonitoringDelivery'][0]['VehicleActivity'])
+    # Parsing through lists and dictionaries in the input json file
+    try:
+        vehicleMonitoring = (
+            busData['Siri']['ServiceDelivery']
+            ['VehicleMonitoringDelivery'][0]['VehicleActivity'])
+    except KeyError:
+        print "Error: No such route"
+        sys.exit()
 
-    print "Bus Line is:  %s" % sys.argv[2]
+    # Printing the Bus Line and # Active Buses output
+    print "Bus Line is: %s" % sys.argv[2]
     print "Number of Active Buses: %d" % len(vehicleMonitoring)
 
+    # Creating an empty list to store active buses location attributes
     busLoc = []
 
     for bus in vehicleMonitoring:
@@ -24,6 +34,7 @@ LineRef=%s' % (sys.argv[1], sys.argv[2])
             if "MonitoredVehicleJourney" in key:
                 busLoc.append(bus[key]['VehicleLocation'])
 
+    # Printing the output of Bus Location list object through iterations
     for i in range(len(busLoc)):
         busLat = busLoc[i]['Latitude']
         busLon = busLoc[i]['Longitude']
